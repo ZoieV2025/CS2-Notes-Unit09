53166ed4d2007576b5fb7103972653f5908959bd
ZoieV2025 <zoie.vitale2025@gbwl.org>
2025-03-06 13:53:51 +0000

    updated java docs

--- Car.java
+++ Car.java
@@ -2,5 +2,24 @@
 "?Car IS-A type of Vehicle"
 */
 public class Car extends Vehicle{
+    // Attributes that are SPECIFIC to
+    // a Car but not all Vehicles
+    private int numSeats;
+    private String brand;
+
+    // Constructors are NOT inheritted!!!
+    public Car() {
+        super(); // CALL to the superclass constructor
+        this.numSeats = 5;
+        this.brand = "BMW";
+    }
+
+    public Car(int numWheels; double avgSpeed; String color; int numSeats; String brand) {
+        // Must call super() FIRST
+        // before setting up the other vars
+        super(numWheels, avgSpeed, color);
+        this.numSeats = numSeats;
+        this.brand = brand;
+    }
 
 }
--- Vehicle.java
+++ Vehicle.java
@@ -8,5 +8,18 @@ public class Vehicle{
     private double avgSpeed;
     private String color;
     // 2. 
+    // Methods to INITIALIZE all variables
+    public Vehicle() {
+        this.numWheels = 4;
+        this.avgSpeed = 60;
+        this.color = "White";
+    }
+    // Constructor with formal parameters
+    public Vehicle(int numWheels; double avgSpeed; String color) {
+        // INITIALIZE to passed arguments
+        this.numWheels = numWheels;
+        this.avgSpeed = avg.Speed;
+        this.color = color;
+    }
 
 }
